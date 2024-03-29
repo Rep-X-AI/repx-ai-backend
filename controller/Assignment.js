28b145027a7cd82066005b79c2ed4c60f6f6afe1
SatyakiDey75 <118485043+SatyakiDey75@users.noreply.github.com>
2024-03-30 00:15:59 +0530

    update and delete assignment done

--- controller/Assignment.js
+++ controller/Assignment.js
@@ -36,12 +36,36 @@ exports.fetchAssignmentsOfStudent = async (req, res) => {
     }
 };
 
+exports.deleteAssignment = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const assignment = await Assignment.findByIdAndDelete(id);
+        res.status(200).json({ "assignment": assignment, "status": "deleted" });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
 exports.updateAssignment = async (req, res) => {
     const { id } = req.params;
     try {
-      const assignment = await Assignment.findByIdAndUpdate(id, req.body, { new: true });
-      res.status(200).json(assignment);
+        const assignment = await Assignment.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json({ "assignment": assignment, "status": "updated" });
     } catch (err) {
-      res.status(400).json(err);
+        res.status(400).json(err);
     }
-  };
+};
+
+exports.deleteAllSubmissions = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await Assignment.updateOne({ _id: id }, { $set: { submissions: [] } });
+        if (result.modifiedCount > 0) {
+            return res.status(200).json({ message: 'Submissions deleted successfully' });
+        } else {
+            return res.status(404).json({ message: 'No submissions to delete or assignment not found' });
+        }
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
--- routes/Assignment.js
+++ routes/Assignment.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createAssignment, updateAssignment, fetchAssignmentsOfTeacher, fetchAssignmentsOfStudent } = require("../controller/Assignment");
+const { createAssignment, updateAssignment, fetchAssignmentsOfTeacher, fetchAssignmentsOfStudent, deleteAssignment, deleteAllSubmissions } = require("../controller/Assignment");
 
 router.post("/create", createAssignment)
-      .patch('/:id', updateAssignment)
       .get("/teachers/:useruid", fetchAssignmentsOfTeacher)
-      .get("/students/:useruid", fetchAssignmentsOfStudent);
+      .get("/students/:useruid", fetchAssignmentsOfStudent)
+      .patch('/:id', updateAssignment)
+      .delete('/:id', deleteAssignment)
+      .delete('/:id/submissions', deleteAllSubmissions);;
 
 
 exports.router = router;
