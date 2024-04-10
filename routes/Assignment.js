0683eae679580b6edc9b5c7ddf05ceaaa977baec
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2024-04-10 19:20:13 +0530

    individual assignment details fetch

--- routes/Assignment.js
+++ routes/Assignment.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const { createAssignment, updateAssignment, fetchAssignmentsOfTeacher, fetchAssignmentsOfStudent, deleteAssignment, deleteAllSubmissions, joinAssignment } = require("../controller/Assignment");
 
-router.post("/create", createAssignment)
-      .post("/join", joinAssignment)
-      .get("/teachers/:useruid", fetchAssignmentsOfTeacher)
-      .get("/students/:useruid", fetchAssignmentsOfStudent)
-      .patch('/:id', updateAssignment)
-      .delete('/:id', deleteAssignment)
-      .delete('/:id/submissions', deleteAllSubmissions);;
+const {
+  createAssignment,
+  updateAssignment,
+  fetchAssignmentsOfTeacher,
+  fetchAssignmentsOfStudent,
+  deleteAssignment,
+  deleteAllSubmissions,
+  joinAssignment,
+  fetchParticularAssignmentofStudent,
+  fetchParticularAssignmentofTeacher,
+} = require("../controller/Assignment");
 
 
-exports.router = router;
+router
+  .post("/create", createAssignment)
+  .post("/joinAssignment",joinAssignment)
+  .get("/teachers/:useruid", fetchAssignmentsOfTeacher)
+  .get("/students/:useruid", fetchAssignmentsOfStudent)
+  .patch("/:id", updateAssignment)
+  .delete("/:id", deleteAssignment)
+  .delete("/:id/submissions", deleteAllSubmissions)
+  .get("/get-assignment-teacher/:id",fetchParticularAssignmentofTeacher)
+  .get("/get-assignment-student/:id",fetchParticularAssignmentofStudent)
+
+module.exports = {router}
