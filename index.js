b1d4d192ebdff78bf68e904cab31d0a0c33c4f80
ArunavaCoderEm <acecankill@gmail.com>
2024-05-11 17:51:47 +0530

    (PROCHUR VODRO KOYEKTA VASA) ml delete creaete done !!

--- index.js
+++ index.js
@@ -22,6 +22,58 @@ server.use(
   })
 );
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', corsOrigin);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
+server.post('/deleteAssignment', async (req, res) => {
+  try {
+    const requestData = req.body; 
+    const response = await axios.post('http://35.193.123.233/deleteAssignment', requestData);
+    res.json(response.data);
+  } catch (error) {
+    if (error.response) {
+      res.status(error.response.status).send(error.response.statusText);
+    } else {
+      res.status(500).send('Internal Server Error');
+    }
+  }
+});
+
+
+server.post('/createAssignment', async (req, res) => {
+  try {
+    const requestData = req.body; 
+    const response = await axios.post('http://35.193.123.233/createAssignment', requestData);
+    res.json(response.data);
+  } catch (error) {
+    if (error.response) {
+      res.status(error.response.status).send(error.response.statusText);
+    } else {
+      res.status(500).send('Internal Server Error');
+    }
+  }
+});
+
+
+// server.post('/evaluateAssignment', async (req, res) => {
+//   try {
+//     const requestData = req.body; 
+//     const response = await axios.post('http://35.193.123.233/evaluateAssignment', requestData);
+//     res.json(response.data);
+//   } catch (error) {
+//     if (error.response) {
+//       res.status(error.response.status).send(error.response.statusText);
+//     } else {
+//       res.status(500).send('Internal Server Error');
+//     }
+//   }
+// });
+
+
 // Connect to database
 connectToDB().catch(err => console.log(err));
 async function connectToDB(){
