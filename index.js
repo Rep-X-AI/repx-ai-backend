be31fb84dee8031baa3872e8c41af8976a94bb37
ArunavaCoderEm <acecankill@gmail.com>
2024-05-11 18:07:12 +0530

    check check

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server.use((req, res, next) => {
 });
 
 
-server.post('/deleteAssignment', async (req, res) => {
+server.post(`${corsOrigin}/deleteAssignment`, async (req, res) => {
   try {
     const requestData = req.body; 
     const response = await axios.post('http://35.193.123.233/deleteAssignment', requestData);
@@ -44,7 +44,7 @@ server.post('/deleteAssignment', async (req, res) => {
 });
 
 
-server.post('/createAssignment', async (req, res) => {
+server.post(`${corsOrigin}/createAssignment`, async (req, res) => {
   try {
     const requestData = req.body; 
     const response = await axios.post('http://35.193.123.233/createAssignment', requestData);
